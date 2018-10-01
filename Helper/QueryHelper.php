8ae9aee9a7c5e6685bff6609d9490e1daf82acab
Alexander Keil <alexander.keil80@gmail.com>
2018-10-01 16:24:06 +0200

    QueryHelper more functions

--- Helper/QueryHelper.php
+++ Helper/QueryHelper.php
@@ -2,6 +2,8 @@
 
 namespace Rawburner\Helper;
 
+use Doctrine\Common\Collections\Criteria;
+
 /**
  * Class QueryHelper
  * @author Alexander Keil (alexanderkeil@leik-software.com)
@@ -23,5 +25,26 @@ class QueryHelper
                 '(%s.active_to IS NULL OR %s.active_to IS NOT NULL AND %s.active_to < %s))',
                 $tableAlias, $tableAlias, $tableAlias, '"'.$today.'"', $tableAlias, $tableAlias, $tableAlias, '"'.$today.'"'
             );
+    }
+
+    /**
+     * @author Alexander Keil
+     * @return Criteria
+     */
+    public static function getVisibleCriteria(): Criteria{
+        $criteria = Criteria::create()
+            ->where(Criteria::expr()->eq('deleted', false))
+            ->andWhere(Criteria::expr()->eq('active', true));
+        return $criteria;
+    }
+
+    /**
+     * @author Alexander Keil
+     * @return Criteria
+     */
+    public static function getActiveCriteria(): Criteria{
+        $criteria = Criteria::create()
+            ->where(Criteria::expr()->eq('deleted', false));
+        return $criteria;
     }
 }
