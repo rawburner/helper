36947e2493401676997679b3b0663063c0ff6f83
Alexander Keil <alexander.keil80@gmail.com>
2018-11-02 18:31:52 +0100

    refactoring DateHelper and fixed return of Converter

--- Helper/DateHelper.php
+++ Helper/DateHelper.php
@@ -5,43 +5,38 @@ namespace Rawburner\Helper;
 use Assert\Assertion;
 
 /**
- * Class DateHelper
  * @author Alexander Keil (alexanderkeil@leik-software.com)
- * @package Rawburner\Helper
  */
 class DateHelper
 {
     /**
-     * Ermittelt die Werktage zwischen zwei Datumswerten
+     * Ermittelt die Werktage zwischen zwei Datumswerten.
+     *
      * @param $startDate
      * @param $endDate
-     * @author Alexander Keil
-     * @return array|\DateTime[]
-     * @throws \Exception
      */
-    public static function getWeekdaysBetweenDates($startDate, $endDate){
+    public static function getWeekdaysBetweenDates($startDate, $endDate):array
+    {
         $weekDays = [];
         $period = new \DatePeriod(
             self::convertDateStringToObject($startDate),
             new \DateInterval('P1D'),
-            self::convertDateStringToObject($endDate)->setTime(23,59,59)
+            self::convertDateStringToObject($endDate)->setTime(23, 59, 59)
         );
 
         /** @var \DateTime $date */
-        foreach ($period as $date){
-            if(in_array($date->format('N'), [6,7])){
+        foreach ($period as $date) {
+            if (\in_array($date->format('N'), [6, 7], true)) {
                 continue;
             }
-            $weekDays[]=$date;
+            $weekDays[] = $date;
         }
+
         return $weekDays;
     }
 
-    /**
-     * @param $number
-     * @return mixed
-     */
-    public static function getMonthGermanFormat($number){
+    public static function getMonthGermanFormat(string $number):string
+    {
         $arMonths = [
             1 => 'Januar',
             2 => 'Februar',
@@ -54,8 +49,7 @@ class DateHelper
             9 => 'September',
             10 => 'Oktober',
             11 => 'November',
-            12 => 'Dezember'
-
+            12 => 'Dezember',
         ];
 
         return $arMonths[$number];
@@ -64,65 +58,62 @@ class DateHelper
     /**
      * @param $date
      * @param string $format
-     * @author Alexander Keil (alexanderkeil80@gmail.com)
-     * @return bool
+     *
      * @see https://stackoverflow.com/questions/19271381/correctly-determine-if-date-string-is-a-valid-date-in-that-format
      */
-    public static function isDateValid($date, $format = 'Y-m-d')
+    public static function isDateValid($date, $format = 'Y-m-d'): bool
     {
         $d = \DateTime::createFromFormat($format, $date);
+
         return $d && $d->format($format) === $date;
     }
 
     /**
-     * @param $date
-     * @param string $format
-     * @author Alexander Keil (alexanderkeil80@gmail.com)
-     * @return bool
+     * @param string|\DateTime $date
      */
-    public static function isDateInFuture($date){
+    public static function isDateInFuture($date): bool
+    {
         $current_date = new \DateTime();
-        $current_date->setTime(0,0,0);
+        $current_date->setTime(0, 0, 0);
         $check_date = self::convertDateStringToObject($date);
-        $check_date->setTime(0,0,0);
+        $check_date->setTime(0, 0, 0);
 
         return $check_date > $current_date;
     }
 
     /**
-     * Datum vergleichen
-     * @param $datetime1
-     * @param $datetime2
-     * @param string $format
-     * @return bool
+     * @param string|\DateTime $datetime1
+     * @param string|\DateTime $datetime2
      */
-    public static function isSameDateTime($datetime1, $datetime2, $format = 'd.m.Y'){
+    public static function isSameDateTime($datetime1, $datetime2, ?string $format = 'd.m.Y'): bool
+    {
         $datetime1 = self::convertDateStringToObject($datetime1);
         $datetime2 = self::convertDateStringToObject($datetime2);
-        if(!$datetime1 || $datetime2){
+        if (!$datetime1 || $datetime2) {
             return false;
         }
-        return $datetime1->format($format) == $datetime2->format($format);
+
+        return $datetime1->format($format) === $datetime2->format($format);
     }
 
     /**
-     * Konvertiert einen String in ein DateTime-Objekt
-     * @param $dateString
-     * @author Alexander Keil (alexanderkeil80@gmail.com)
-     * @return bool|\DateTime|null
+     * @param \DateTime|string $dateString
      */
-    public static function convertDateStringToObject($dateString){
-        if($dateString instanceof \DateTime){
+    public static function convertDateStringToObject($dateString): ?\DateTime
+    {
+        if ($dateString instanceof \DateTime) {
             return $dateString;
         }
-        try{
-            Assertion::notEmpty($dateString);
+
+        try {
+            Assertion::notEmpty($dateString, sprintf('String given in "%s" is empty', __METHOD__));
             $timestamp = strtotime($dateString);
-            Assertion::notEq($timestamp, false, 'Datumsangabe konnte nicht validiert werden: '.$dateString);
+            Assertion::notEq($timestamp, false, sprintf('DateString is not valid: "%s"', $dateString));
             $dateTime = new \DateTime();
             $dateTime->setTimestamp($timestamp);
+
             return $dateTime;
-        }catch (\Throwable $exception){
+        } catch (\Throwable $exception) {
             return null;
         }
     }
